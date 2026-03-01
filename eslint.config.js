e5b565f9bea4c808a1e1dd7f785a942f007a5a57
Chrystalio <chrystalio.k@outlook.com>
2026-03-01 21:07:32 +0700

    build: add eslint config and lint script

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+module.exports = [{
+    languageOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'commonjs',
+        globals: {
+            console: 'readonly',
+            process: 'readonly',
+            Buffer: 'readonly',
+            __dirname: 'readonly',
+            __filename: 'readonly',
+            module: 'readonly',
+            require: 'readonly',
+            exports: 'readonly'
+        }
+    },
+    rules: {
+        'no-unused-vars': 'warn',
+        'no-console': 'off'
+    }
+}];
